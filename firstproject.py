fa5cde76ae4094f7a7856f00fd3d7976800f0962
Bishal Aryal <bishal.dmarketer@gmail.com>
2022-06-24 08:16:51 +0545

    first commit

---
+++ firstproject.py
@@ -0,0 +1,7 @@
+a = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+print(f'Hello Sir {a} \n Good Morning')
+if age >=18:
+	print("You are eligible for vote.")
+else:
+	print("Grow up young man :)")
